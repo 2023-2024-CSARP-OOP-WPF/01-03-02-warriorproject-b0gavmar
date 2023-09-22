ca360df1a3a6f5441f77691f9e9600437182ace2
b0gavmar <b0gavmar@vasvari.org>
2023-09-22 13:32:04 +0200

    nem mukszik

--- WarriorProject/Program.cs
+++ WarriorProject/Program.cs
@@ -6,11 +6,67 @@ using System;
 
 namespace WarriorProject
 {
+
+    public class Position
+    {
+        private int x; 
+        private int y;
+
+        public int Xxd { get { return x; } }
+        public int Yxd { get { return y; } }
+
+        public Position(int x, int y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+
+        public void ToConsole()
+        {
+            Console.WriteLine($"({x},{y})");
+        }
+    
+    }
+
+    public class Warrior
+    {
+        public string name;
+        public Position position;
+
+        public Warrior(string name, Position p)
+        {
+            this.name = name;
+            this.position = p;
+        }
+
+        public void MoveHorizontal(int distance)
+        {
+            this.position.Xxd += distance;
+
+        }
+
+        public void MoveVertical(int distance)
+        {
+
+        }
+
+        public void ToConsole()
+        {
+            Console.WriteLine($"");
+        }
+    }
+
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Position position = new Position(2, -1);
+            position.ToConsole();
+            Warrior warrior = new Warrior("Peter the Iron Man", position);
+            warrior.ToConsole();
+            warrior.MoveHorizontal(3);
+            warrior.MoveVertical(-2);
+            warrior.ToConsole();
         }
     }
 }
